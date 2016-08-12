487dfdd9b24a793487137e2419ca1edf4d914610
Claire <guerin.claire01@gmail.com>
2016-08-12 11:36:43 +0200

    Finalized data_analysis.r and created mutli_lm.r

---
+++ multi_lm.R
@@ -0,0 +1,13 @@
+###############################################
+###                         				###
+### 		BAT IBM SENSORY JAMMING 		###
+###		INFERENTIAL STATISTICS			### 
+###                         				###
+###############################################
+
+### Created on 12/08/2016 at 11:33
+### Author: Claire Guérin
+
+rm(list = ls())
+# clear current workspace
+
